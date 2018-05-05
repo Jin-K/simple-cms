a3847c3567b15f9ff24eec2d823fe0d302285211
Jin-K <angelmnz@outlook.com>
2018-05-05 14:08:10 +0200

    added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  answer: string = '';
-  answerDisplay: string = '';
-  showSpinner: boolean = false;
-
-  showAnswer() {
-    this.showSpinner = true;
-
-    setTimeout(() => {
-      this.answerDisplay = this.answer;
-      this.showSpinner = false;
-    }, 2000);
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@
  * Things to know:
  *    BrowserAnimationsModule¹: 
  *      WebAnimation API isn't supported by all browsers.  If you want to support Material component animations in these browsers, you'll have to include a polyfill( https://github.com/web-animations/web-animations-js )
- *      If you don't want to add another dependency to your project, you can use the NoopAnimationsModule
+ *      If you don't want to have animations in your project, you should replace BrowserAnimationsModule by NoopAnimationsModule
  *    MatxxxxxxModule¹: 
  *      Material Modules (and/or components) to use in our app
  */
@@ -13,19 +13,24 @@ import { BrowserModule }            from '@angular/platform-browser';
 import { BrowserAnimationsModule }  from '@angular/platform-browser/animations'
 import { NgModule }                 from '@angular/core';
 import { FormsModule }              from '@angular/forms';
+
 import { AppComponent }             from './app.component';
+import { Routing }                  from './app.routing';
 import { MaterialModule }           from './material.module';
+import { HomeComponent }            from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    Routing
   ],
   providers: [],
   bootstrap: [ AppComponent ]
