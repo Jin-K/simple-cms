a310a23988281db58aebcf316ecaf0f1c8c37178
Jin-K <angelmnz@outlook.com>
2018-05-05 16:21:41 +0200

    Drawer (Sidenav) added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,15 +4,17 @@ import {
   MatToolbarModule,
   MatInputModule,
   MatProgressSpinnerModule,
-  MatCardModule
+  MatCardModule,
+  MatIconModule,
+  MatSidenavModule
 }                           from '@angular/material';
 import { NgModule }         from '@angular/core';
 import { CommonModule }     from '@angular/common';
 
 
 @NgModule({
-  imports: [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule ],
-  exports: [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule ]
+  imports: [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatIconModule, MatSidenavModule ],
+  exports: [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatIconModule, MatSidenavModule ]
 })
 
 export class MaterialModule {}
