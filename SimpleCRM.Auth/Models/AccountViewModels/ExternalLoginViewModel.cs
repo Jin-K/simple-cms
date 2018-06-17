7c687571ae353b8bef8ad281ab6035f52d273f7c
Jin-K <angelmnz@outlook.com>
2018-06-17 20:48:08 +0200

    Copy & Paste (External Login: Microsoft Account)

---
+++ SimpleCRM.Auth/Models/AccountViewModels/ExternalLoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCRM.Auth.Models.AccountViewModels {
+  public class ExternalLoginViewModel {
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+  }
+}
