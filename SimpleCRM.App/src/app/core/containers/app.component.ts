1fb7185b39436fc89e1fd732de04dd9dfd775305
Jin-K <angelmnz@outlook.com>
2018-06-24 13:48:53 +0200

    Affiche pseudo d'user connecté

--- SimpleCRM.App/src/app/core/containers/app.component.ts
+++ SimpleCRM.App/src/app/core/containers/app.component.ts
@@ -13,12 +13,19 @@ export class AppComponent implements OnInit, OnDestroy {
   isAuthorizedSubscription: Subscription;
   isAuthorized = false;
 
+  hasAdminRole = false
+  hasDataEventRecordsAdminRole = false;
+  userDataSubscription: Subscription;
+  userData: any;
+
+  givenName: string;
+
   constructor(private oidcSecurityService: OidcSecurityService) {
     if (this.oidcSecurityService.moduleSetup) this.doCallbackLogicIfRequired();
     else this.oidcSecurityService.onModuleSetup.subscribe(() => this.doCallbackLogicIfRequired());
   }
 
-  // Todo: Déplacer ça (login, logout) ailleurs non ?
+  // Todo: Déplacer ça (login, logout) ailleurs non ? ==> dans un navigation.component comme SecurityAspNetCoreSignalR
   login() {
     console.log('Do login logic');
     this.oidcSecurityService.authorize();
@@ -31,10 +38,31 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.isAuthorizedSubscription = this.oidcSecurityService.getIsAuthorized().subscribe(isAuthorized => this.isAuthorized = isAuthorized);
+
+    this.userDataSubscription = this.oidcSecurityService.getUserData().subscribe(
+      userData => {
+        if (userData && userData !== '' && userData.role) {
+          this.givenName = userData.given_name;
+          for (let i = 0; i < userData.role.length; i++) {
+            switch (userData.role[i]) {
+              case 'dataEventRecords.admin':
+                this.hasDataEventRecordsAdminRole = true;
+                break;
+              case 'admin':
+                this.hasAdminRole = true;
+                break;
+            }
+          }
+        }
+
+        console.log('userData getting data');
+      }
+    );
   }
 
   ngOnDestroy() {
     if (this.isAuthorizedSubscription) this.isAuthorizedSubscription.unsubscribe();
+    if (this.userDataSubscription) this.userDataSubscription.unsubscribe();
     this.oidcSecurityService.onModuleSetup.unsubscribe();
   }
 
