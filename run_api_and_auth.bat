6d73e876b035ea9990bf9d464e44f3f62fe75c59
Jin-K <angelmnz@outlook.com>
2018-07-18 00:50:08 +0200

    chipo pour lancer depuis VSCode

---
+++ run_api_and_auth.bat
@@ -0,0 +1,3 @@
+start dotnet run --project SimpleCRM.Api\SimpleCRM.Api.csproj
+start dotnet run --project SimpleCRM.Auth\SimpleCRM.Auth.csproj
+start cmd.exe /c npm run --prefix SimpleCRM.App watch-webpack-dev
