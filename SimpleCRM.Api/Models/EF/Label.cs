8159dfd76bdfbe900bf3966f58bee978100df6d8
Jin-K <angelmnz@outlook.com>
2018-08-14 23:01:13 +0200

    SimpleCRM.Api: Updated nuget packages & added Microsoft.EntityFramework.Design

--- SimpleCRM.Api/Models/EF/Label.cs
+++ SimpleCRM.Api/Models/EF/Label.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace SimpleCRM.Api.Models.EF {
   public class _Label {
-    [Key]
     public int Id { get; set; }
     public string Label { get; set; }
     public System.DateTime Created { get; set; }
